0dab4459f59c72291548c4934bd47b9c5126a8ea
mojtaba-khallash <mojkh1385@yahoo.com>
2020-06-07 21:15:43 +0430

    feat(i18n): Use "translate" service
    Translate all the UI messages returned from rules and in generated UI by
    linter.
    
    Add a test case

--- test/spec/DrdLintingSpec.js
+++ test/spec/DrdLintingSpec.js
@@ -62,3 +62,86 @@ describe('linting', function() {
     expect(config).to.eql(dmnlintrc);
   });
 });
+
+describe('i18n', function() {
+
+  let container, editor;
+  const translations = {
+    'Toggle linting': 'تغییر وضعیت عیب یابی',
+    'Element is missing label/name': 'المان فاقد برچسب/نام است',
+    '{errors} Errors, {warnings} Warnings': '{errors} خطا, {warnings} هشدار'
+  };
+
+  beforeEach(function(done) {
+
+    container = TestContainer.get(this);
+
+    const diagram = require('./diagram.dmn');
+
+    function translateModule(template, replacements = {}) {
+      // Translate
+      let transTemplate = translations[template] || template;
+
+      // Replace
+      return transTemplate.replace(/{([^}]+)}/g, function(_, key) {
+        return key in replacements ? replacements[key] : '{' + key + '}';
+      });
+    }
+
+    const modeler = new Modeler({
+      container: container,
+      drd: {
+        additionalModules: [
+          DrdLinting,
+          {
+            translate: [ 'value', translateModule ]
+          }
+        ]
+      },
+      common: {
+        linting: dmnlintrc
+      }
+    });
+
+    modeler.importXML(diagram, function(error) {
+      if (error) {
+        return done(error);
+      }
+
+      editor = modeler.getActiveViewer();
+
+      done();
+    });
+  });
+
+  it('should translate lint issues text', function(done) {
+
+    // when
+    const linting = editor.get('drdLinting'),
+          eventBus = editor.get('eventBus');
+
+    linting.toggle(true);
+
+    // then
+    eventBus.once('linting.completed', function(event) {
+
+      const button = container.querySelector('button.djsl-button.djsl-button-error');
+      expect(button).to.exist;
+      expect(button.title).to.equal(translations['Toggle linting']);
+
+      const buttonTextSpan = button.querySelector('span');
+      expect(buttonTextSpan).to.exist;
+      let expected = translations['{errors} Errors, {warnings} Warnings']
+        .replace('{errors}', 1)
+        .replace('{warnings}', 0);
+      expect(buttonTextSpan.innerText).to.equal(expected);
+
+      const shapeLabelRequiredMessage = container.querySelector('a[data-rule="label-required"]');
+      expect(shapeLabelRequiredMessage).to.exist;
+      expect(shapeLabelRequiredMessage.dataset.message).to.equal(translations['Element is missing label/name']);
+
+      done();
+    });
+  });
+
+});
